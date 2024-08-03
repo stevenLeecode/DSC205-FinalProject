675e0f5b65004ca8b8121f6f12fd5d893e9ffa95
stevenLeecode <158061583+stevenLeecode@users.noreply.github.com>
2024-08-03 07:53:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-player_total = pd.read_csv('https://raw.githubusercontent.com/stevenLeecode/DSC205-FinalProject/main/Player%20Totals.csv')
+#https://raw.githubusercontent.com/stevenLeecode/DSC205-FinalProject/main/Player%20Totals.csv
+player_total = pd.read_csv('Player Totals.csv')
 
 st.title('NBA Greatest of All Time Comparisons')
 st.subheader('LeBron James, Michael Jordan, Kareem Abdul-Jabbar, Kobe Bryant, Stephen Curry')
