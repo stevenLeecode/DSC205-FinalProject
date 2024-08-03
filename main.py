0042c447db4249f6132cda721f12a7de39214502
stevenLeecode <158061583+stevenLeecode@users.noreply.github.com>
2024-08-03 08:39:17 -0400

    Refactoring header

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 #https://raw.githubusercontent.com/stevenLeecode/DSC205-FinalProject/main/Player%20Totals.csv
 player_total = pd.read_csv('Player Totals.csv')
 
-st.title('NBA Greatest of All Time Comparisons')
-st.subheader('Dataset: https://www.kaggle.com/datasets/rodneycarroll78/nba-stats-1980-2024?select=Player+Season+Info.csv ')
+st.title('NBA Greatest of All Time Comparisons - Steven Lee')
+st.subheader('NBA Players Dataset')
 
 st.dataframe(player_total, width = 600, height = 200)
