caf5fa50aee60c58597b58f8842c31828d346a14
stevenLeecode <158061583+stevenLeecode@users.noreply.github.com>
2024-08-03 07:21:20 -0400

    Added main python file

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+player_total = pd.read_csv('https://raw.githubusercontent.com/stevenLeecode/DSC205-FinalProject/main/Player%20Totals.csv')
+
+st.title('NBA Greatest of All Time Comparisons')
+st.subheader('LeBron James, Michael Jordan, Kareem Abdul-Jabbar, Kobe Bryant, Stephen Curry')
