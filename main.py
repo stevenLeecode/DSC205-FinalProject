d48db3a116f0fe1d782ac2bd3596925a28f234aa
stevenLeecode <158061583+stevenLeecode@users.noreply.github.com>
2024-08-05 11:07:49 -0400

    forgot pages

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ player_total = pd.read_csv('Player Totals.csv')
 
 #st.title('NBA Greatest of All Time Comparisons - Steven Lee')
 st.markdown("<h2 style = 'text-align: center'> NBA Greatest Player of All Time Comparisons <br> <h2>", unsafe_allow_html=True)
-st.page_link("1_🏀_Offensive_Stats.py", label = "Offensive Stats")
-st.page_link("2_🏀_Defensive_Stats.py", label = "Defensive Stats")
+st.page_link("pages/1_🏀_Offensive_Stats.py", label = "Offensive Stats")
+st.page_link("pages/2_🏀_Defensive_Stats.py", label = "Defensive Stats")
 
 player_html = """
 <h3 style = 'text-align: center;'> 5 Players Analyzed </h3>
