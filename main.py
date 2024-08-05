0f7b12f55ca55ec5a1b026632b5dc9d9f0b949c5
stevenLeecode <158061583+stevenLeecode@users.noreply.github.com>
2024-08-05 10:10:21 -0400

    change

--- main.py
+++ main.py
@@ -17,4 +17,3 @@ st.markdown("<h2 style = 'text-align: center'> NBA Greatest Player of All Time C
 st.markdown("<h3 style = 'text-align: center'> NBA Players Dataset </h3>", unsafe_allow_html=True)
 
 st.dataframe(player_total, width = 1400, height = 275)
-
