76b41413c1bcbf5130dedebc79b220612ede297c
stevenLeecode <158061583+stevenLeecode@users.noreply.github.com>
2024-08-07 06:59:41 -0400

    Color changes

--- pages/1_🏀_Offensive_Stats.py
+++ pages/1_🏀_Offensive_Stats.py
@@ -77,10 +77,10 @@ ax = fig.add_subplot()
 ax.set_xticks(lbj.index)
 ax.set_xticklabels(labels)
 
-ax.plot(lbj.index, lbj['pts'], label = 'LeBron James', color = 'gold', linestyle = 'dotted')
+ax.plot(lbj.index, lbj['pts'], label = 'LeBron James', color = 'peru', linestyle = 'dotted')
 ax.plot(mj.index, mj['pts'], label = 'Michael Jordan', color = 'red')
-ax.plot(kareem.index, kareem['pts'], label = 'Kareem Abdul-Jabbar', color = 'green')
-ax.plot(kobe.index, kobe['pts'], label = 'Kobe Bryant', color = 'blue')
+ax.plot(kareem.index, kareem['pts'], label = 'Kareem Abdul-Jabbar', color = 'dimgrey')
+ax.plot(kobe.index, kobe['pts'], label = 'Kobe Bryant', color = 'lavender')
 ax.plot(curry.index, curry['pts'], label = 'Stephen Curry', color = 'purple')
 
 ax.set_xlabel('Year')
